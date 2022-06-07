c3d4b748d9fab22c56a4d6f297d9fe8128f4b9b0
Istvan Varadi <Istvan@Amsos-MacBook-Pro.local>
2022-06-07 20:17:33 +0200

    fix:fixing navbar

--- pages/pets/index.js
+++ pages/pets/index.js
@@ -1,17 +1,17 @@
 const hamburgerButton = document.getElementById('hamburger')
 const navList = document.getElementById('nav-list')
 const logo = document.getElementById('logo')
-
+const header = document.querySelector('header')
 function toggleButton() {
     if (navList.style.display === 'none') {
         navList.style.display = 'block'
         hamburgerButton.classList.toggle('rotate')
         hamburgerButton.style = ' margin-right: 4rem;'
+        header.style.height = '100vh'
     } else {
         navList.style.display = 'none'
         hamburgerButton.classList.remove('rotate')
         hamburgerButton.style = ' margin-right: 0;'
     }
 }
-
 hamburgerButton.addEventListener('click', toggleButton)
